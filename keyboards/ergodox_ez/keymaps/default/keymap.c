da551331abe393bb162884caf52621b8a53abe06
石井 健太 <kishii@microcircus.com>
2019-01-24 15:49:05 +0900

    layer1 keymap changed.

--- keyboards/ergodox_ez/keymaps/default/keymap.c
+++ keyboards/ergodox_ez/keymaps/default/keymap.c
@@ -15,22 +15,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | `      |   1  |   2  |   3  |   4  |   5  | PrtSC|           |E to J|   6  |   7  |   8  |   9  |   0  |   -    |
+ * | `      |   1  |   2  |   3  |   4  |   5  | PrtSC|           |  | \ |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  Esc |   Y  |   U  |   I  |   O  |   P  | BackSP |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  None  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ; + |  Enter |
- * |--------+------+------+------+------+------|Ctrl +|           |  B   |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |  F3  |           |      |   N  |   M  |   ,  |   .  | / ?  | RShift |
+ * |  Ctrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ; + |  Enter |
+ * |--------+------+------+------+------+------| Left |           | Right|------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  | / ?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | Ctrl | Win  | LAlt | F5   | @    |                                       |  ^ ~ | : *  | [ {  |  ] } | \ _  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.        ,-------------.
- *                                        |PageUP|PageDW|       | Left | Right  |
+ *                                        |  Up  | Down |       | Left | Right  |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       |  Up  |        |      |
- *                                 | Space|  F2  |------|       |------|  Del   |RShift|
- *                                 |      |      | End  |       | Down |        |      |
+ *                                 |      |      | Home |       |PageUP|        |      |
+ *                                 | Space|  F2  |------|       |------|  Del   | Ctrl |
+ *                                 |      |      | End  |       |PageDW|        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -39,21 +39,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_PSCREEN,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
-        KC_TRNS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   LCTL(KC_F3),
+        KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LEFT,
         KC_LCTL,        KC_LGUI,      KC_LALT,KC_F5,  KC_LBRC,
-                                               KC_PGUP, KC_PGDN,
+                                               KC_UP, KC_DOWN,
                                                               KC_HOME,
                                                KC_SPC,KC_F2,    KC_END,
         // right hand
-          LALT(KC_LSFT),KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
+          KC_JYEN,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
           KC_ESC,      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSPC,
                        KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_ENT,
-          KC_B,        KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
+          KC_RGHT,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                                KC_EQL, KC_QUOT,KC_RBRC,KC_BSLS,          KC_RO,
           KC_LEFT,        KC_RGHT,
-          KC_UP,
-          KC_DOWN,KC_DEL, KC_RSFT
+          KC_PGUP,
+          KC_PGDN,KC_DEL, KC_RCTL
     ),
 /* Keymap 1: Symbol Layer
  *
